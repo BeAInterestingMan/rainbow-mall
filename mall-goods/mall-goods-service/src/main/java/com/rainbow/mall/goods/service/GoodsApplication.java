e7c9cbfd93ab78d5e022d612b19ec11d2fc2ca1c
liuhu <1649471814@qq.com>
2022-06-15 21:23:59 +0800

    规划端口号

--- mall-goods/mall-goods-service/src/main/java/com/rainbow/mall/goods/service/GoodsApplication.java
+++ mall-goods/mall-goods-service/src/main/java/com/rainbow/mall/goods/service/GoodsApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.rainbow.mall.goods.service.mapper")
-@EnableFeignClients(basePackages = {"com.rainbow.mall"})
+@EnableFeignClients(basePackages = {"com.rainbow.mall.*"})
 @EnableDiscoveryClient
 public class GoodsApplication {
 
